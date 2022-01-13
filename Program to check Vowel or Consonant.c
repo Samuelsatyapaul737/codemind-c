96e3a6c60f861c858662a0d600e105d7bd8472a4
Samuelsatyapaul737 <96734455+Samuelsatyapaul737@users.noreply.github.com>
2022-01-13 17:43:37 +0530

    #include <stdio.h> int main() {     char c;     int lowercase, uppercase;
        scanf("%c", &c);
    
        lowercase = (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u');
    
       
        uppercase = (c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U');
    
    
        if (lowercase || uppercase)
            printf("Vowel");
        else
            printf("Consonant");
        return 0;
    }

---
+++ Program to check Vowel or Consonant.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main() {
+    char c;
+    int lowercase, uppercase;
+
+    scanf("%c", &c);
+
+    lowercase = (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u');
+
+   
+    uppercase = (c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U');
+
+
+    if (lowercase || uppercase)
+        printf("Vowel");
+    else
+        printf("Consonant");
+    return 0;
+}
