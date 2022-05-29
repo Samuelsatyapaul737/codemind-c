bedb3e63784640b2663f584a77b97642f8fc967e
Samuelsatyapaul737 <96734455+Samuelsatyapaul737@users.noreply.github.com>
2022-05-29 17:37:51 +0530

    #include<stdio.h>
    int main()
    {
        int i, j, rows, columns, a[10][10], Sum;
        
       
        scanf("%d", &i);
        
       
        for(rows = 0; rows < i; rows++)
        {
            for(columns = 0; columns < 2; columns++)
            {
                scanf("%d", &a[rows][columns]);
            }
        }
        
        for(rows = 0; rows < i; rows++)
        {
            Sum = 0;
            for(columns = 0; columns < 2; columns++)
            {
                Sum = Sum + a[rows][columns];
            }
         printf("%d
    ",Sum);
        }
        
        return 0;
    }

---
+++ sum_of_the_given_numbers.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+ 
+int main()
+{
+    int i, j, rows, columns, a[10][10], Sum;
+    
+   
+    scanf("%d", &i);
+    
+   
+    for(rows = 0; rows < i; rows++)
+    {
+        for(columns = 0; columns < 2; columns++)
+        {
+            scanf("%d", &a[rows][columns]);
+        }
+    }
+    
+    for(rows = 0; rows < i; rows++)
+    {
+        Sum = 0;
+        for(columns = 0; columns < 2; columns++)
+        {
+            Sum = Sum + a[rows][columns];
+        }
+     printf("%d
+",Sum);
+    }
+    
+    return 0;
+}
