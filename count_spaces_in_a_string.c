e648bba6ba6a7b8a716a23c79767f78e75ab2c1e
Samuelsatyapaul737 <96734455+Samuelsatyapaul737@users.noreply.github.com>
2022-06-30 06:20:44 +0530

    #include <stdio.h> #include <string.h>
    int  main()
    {
        char s[200],c=0;
        int i;
        scanf("%[^
    ]s", s);
         for(i=0;s[i]!=NULL;i++)
        {
        	if(s[i]==' ')
        	{
        		c++;
    		}
    	}
    	printf("%d",c);
        
        
    }

---
+++ count_spaces_in_a_string.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+ 
+int  main()
+{
+    char s[200],c=0;
+    int i;
+    scanf("%[^
+]s", s);
+     for(i=0;s[i]!=NULL;i++)
+    {
+    	if(s[i]==' ')
+    	{
+    		c++;
+		}
+	}
+	printf("%d",c);
+    
+    
+}
