01aaf94e326ba3d6f20629748b23a640e911c92f
Samuelsatyapaul737 <96734455+Samuelsatyapaul737@users.noreply.github.com>
2022-07-07 15:08:59 +0530

    #include<bits/stdc++.h> using namespace std; int prime(int n) {     if(n==1)     {         return 0;     }     for(int i=2;i*i<=n;i++)     {         if(n%i==0)         {             return 0;         }     }     return 1; } int main() {     int a,b,c=0;     cin>>a>>b;     for(int i=a;i<(b+1);i++)     {         if(prime(i))         {           c++;         }     }     if(c>0)     {         cout<<c;     }     else     {         cout<<"-1";     } }

--- Primes_in_range.c
+++ Primes_in_range.c
@@ -1,37 +1,37 @@
-#include<stdio.h>
-#include<math.h>
-int main() {
-	int i,n,flag,c;
-	int l,r;
-	
-
-	scanf("%d",&l);
-
-	scanf("%d",&r);
-	
-	c = 0; 
-
-	for(n = l;n<=r;++n){
-		
-
-		if(n==1)
-			continue;
-		
-		
-		flag = 0;
-		for(i = 2;i<=sqrt(n);++i){
-			
-			if(n%i==0){
-				flag=1;
-				break;
-			}
-		}
-		
-		if(flag==0)
-			++c;
-			
-	}
-	
-	printf("%d",c);
-	
+#include<bits/stdc++.h>
+using namespace std;
+int prime(int n)
+{
+    if(n==1)
+    {
+        return 0;
+    }
+    for(int i=2;i*i<=n;i++)
+    {
+        if(n%i==0)
+        {
+            return 0;
+        }
+    }
+    return 1;
+}
+int main()
+{
+    int a,b,c=0;
+    cin>>a>>b;
+    for(int i=a;i<(b+1);i++)
+    {
+        if(prime(i))
+        {
+          c++;
+        }
+    }
+    if(c>0)
+    {
+        cout<<c;
+    }
+    else
+    {
+        cout<<"-1";
+    }
 }
